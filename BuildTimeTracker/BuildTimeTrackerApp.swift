fd865b1d4db518a359cdefced14e80dfc8e47c1f
Alexey Gurin <38838291+ElysiumWhale@users.noreply.github.com>
2022-04-23 02:32:45 +0400

    Initial Commit

---
+++ BuildTimeTracker/BuildTimeTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildTimeTrackerApp.swift
+//  BuildTimeTracker
+//
+//  Created by Алексей Гурин on 23.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct BuildTimeTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
